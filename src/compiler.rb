4154669638b6fd1417799154472dc1111741f3cb
Halil Bagdadi <halil.bagdadi@epitech.eu>
2020-04-09 23:47:13 +0200

    Windows ready files

--- src/compiler.rb
+++
@@ -1,6 +0,0 @@
-source_code = File.open(ARGV[0], 'r')
-compiled_code = RubyVM::InstructionSequence.compile(source_code)
-binary_code = compiled_code.to_binary
-binary_file = File.open(ARGV[0].split('.').first + '.bin', 'w+')
-binary_file.puts binary_code
-binary_file.close
--- src/exec.rb
+++
@@ -1,3 +0,0 @@
-executable_binary_code = File.open(ARGV[0], 'r').readlines.join('')
-compiled_instruction_sequence = RubyVM::InstructionSequence.load_from_binary(executable_binary_code)
-compiled_instruction_sequence.eval
